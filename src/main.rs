834dd29554bca92b0dbcd2c80d6b4dc23752e437
Carlos Eduardo Duarte Schwalm <krlsedu@gmail.com>
2020-10-15 00:08:36 -0300

    Primeiro

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,14 @@ mod structs;
 
 fn main() {
     basico::declaracao_basica::contem_warnings();
+
     let user = User { user_name: "Carlos Eduardo".to_string() };
+
     let ide = structs::structs::Ide { name: "Intellij".to_string() };
+
     let fav_user_ide = UserBestIde { user: user.clone(), ide };
     fav_user_ide.show_result();
+
     let mut user_langs = UserLangs {
         user: user.clone(),
         langs: vec![Lang { name: "Java".to_string() }]
