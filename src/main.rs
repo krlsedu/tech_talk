6dc05524dc2575c6ef6c0d05929b121b02aef1a2
Carlos Eduardo Duarte Schwalm <krlsedu@gmail.com>
2020-10-15 19:02:00 -0300

    Cmd

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,16 @@ use std::env;
 
 mod basico;
 mod structs;
-mod cmd;
 
 
 fn main() {
     let args = env::args().collect::<Vec<String>>();
     println!("{:?}",args);
+
     if args.contains(&"-w".to_string()) {
         basico::declaracao_basica::contem_warnings();
     }
+
     for arg in args {
         match arg.as_str() {
             "-w" => basico::declaracao_basica::contem_warnings(),
